c440bfeac4a685ec9904576a16e37e92fcc54e68
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-10-05 17:11:08 +0200

    Delete scope directories

--- sol-cc/contracts/interface/ISolConnector.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.22;
-
-import { AccountDepositSol, AccountWithdrawSol, WithdrawDataSol } from "./ILedger.sol";
-
-interface ISolConnector {
-    function withdraw(WithdrawDataSol calldata data) external;
-}
--- solana-vault/packages/solana/contracts/programs/solana-vault/src/state/vault_state/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod allowed_broker;
-pub mod allowed_token;
-pub mod vault_authority;
-pub use allowed_broker::*;
-pub use allowed_token::*;
-pub use vault_authority::*;
