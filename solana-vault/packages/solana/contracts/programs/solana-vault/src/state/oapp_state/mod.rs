c2b9b39abb3edafa7aa8cd1625816dfb57ac12b2
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-10-13 14:56:18 +0200

    Delete scope directories

--- solana-vault/packages/solana/contracts/programs/solana-vault/src/state/oapp_state/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod enforced_options;
-pub mod peer;
-pub mod oapp_config;
-
-pub use enforced_options::*;
-pub use peer::*;
-pub use oapp_config::*;
--- solana-vault/packages/solana/contracts/programs/solana-vault/src/state/vault_state/allowed_broker.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[account]
-#[derive(InitSpace)]
-pub struct AllowedBroker {
-    pub broker_hash: [u8; 32],
-    pub allowed: bool,
-    pub bump: u8,
-}
